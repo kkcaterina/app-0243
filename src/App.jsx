1ccd2f1f3040015c7085349786e31418d27e5229
rorynmyr <rorynmyr@student.21-school.ru>
2024-10-24 19:39:05 +0300

    add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
-import './App.css';
+import { NavLink, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+const Profile = () => {
+  return <h3>Страница с профилем</h3>;
+};
+const Friends = () => {
+  return <h3>Страница друзей</h3>;
+};
 
 function App() {
   return (
-    <div>
-      <h1>Заголовок</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates, asperiores.</p>
+    <div className="container-fluid pl-5 pr-5 mt-5">
+      <div className="row">
+        <div className="col-sm-4">
+          <div className="nav flex-column nav-pills">
+            <NavLink to="/" className="nav-link">
+              Главная
+            </NavLink>
+            <NavLink to="profile" className="nav-link">
+              Профиль
+            </NavLink>
+            <NavLink to="friends" className="nav-link">
+              Друзья
+            </NavLink>
+            <NavLink to="shop" className="nav-link">
+              Магазин
+            </NavLink>
+            <NavLink to="settings" className="nav-link">
+              Настройки
+            </NavLink>
+          </div>
+        </div>
+        <div className="col-sm-8">
+          <Routes>
+            <Route path="/" element={<h2>Главная страница, выберите из меню слева</h2>}/>
+            <Route path="/profile" element={<Profile />}/>
+            <Route path="/friends" element={<Friends />}/>
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
